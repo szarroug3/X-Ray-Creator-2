e890445558a142c43d5ae0aa3ae6b971b8a30f18
S. Zarroug <szarroug3@gmail.com>
2016-04-01 16:34:53 -0500

    updated to use pywinauto

--- XRayCreator.py
+++ XRayCreator.py
@@ -1,35 +1,132 @@
 # XRayCreator.py
 
+import os
+import argparse
+from kindle.books import Books
 from time import sleep
+from glob import glob
+from shutil import move
 from pywinauto import *
-from book.mobibook import MobiBook
 
-book = MobiBook("test\TFE.mobi")
-book.GetASIN()
-book.UpdateASIN()
-book.GetShelfariInfo()
+def UpdateAll():
+    for book in kindleBooks:
+        MarkForUpdate(book)
+        if book.xrayExists:
+            for xrayFile in glob(os.path.join(book.xrayLocation, '*.asc')):
+                os.remove(xrayFile)
 
-app = Application.start("C:\Users\szarroug3\Documents\X-Ray Builder GUI\X-Ray Builder GUI.exe")
+def Update():
+    print kindleBooks.PrintListOfBooks()
+    books = raw_input("Please enter book number(s) of the book(s) you'd like to update in a comma separated list: ")
+    books = books.replace(" ", "")
+    books = books.split(',')
+    for bookID in books:
+        book = kindleBooks.books[int(bookID) - 1]
+        MarkForUpdate(book)
+        if book.xrayExists:
+            for xrayFile in glob(os.path.join(book.xrayLocation, '*.asc')):
+                os.remove(xrayFile)
 
-mainWindow = app['X-Ray Builder GUI']
-aliasesWindow = app['Aliases']
-chaptersWindow = app['Chapters']
+def New():
+    for book in kindleBooks:
+         if not book.xrayExists:
+            MarkForUpdate(book)
 
-#minimize app
-mainWindow.Minimize()
+def MarkForUpdate(book):    
+    book.update = True
 
-mainWindow['Edit1'].SetEditText(book.bookLocation)
-mainWindow['Edit2'].SetEditText(book.shelfariURL)
-mainWindow['Button6'].Click()	#make sure Source is Shelfari
-mainWindow['Button11'].Click()	#create xray button
+#main
+parser = argparse.ArgumentParser(description='Create and update kindle X-Ray files')
+parser.add_argument('-u', '--update', action='store_true', help='Will give you a list of all books on kindle and asks you to return a list of book numbers for the books you want to update')
+parser.add_argument('-ua', '--updateall', action='store_true',  help='Deletes all X-Ray files and recreates them. Will also create X-Ray files for books that don\'t already have one')
+parser.add_argument('-n', '--new', action='store_true', help='Creates X-Ray files for books that don\'t already have one')
+args = parser.parse_args()
 
-#wait for aliases window
-while not aliasesWindow.Exists():
-	sleep(.5)
-aliasesWindow['No'].Click()
-#wait for chapters window
-while not chaptersWindow.Exists():
-	sleep(.5)
-chaptersWindow['No'].Click()
-app.WaitCPUUsageLower(timeout=30)
-app.kill_()
+#check to make sure only one argument is chosen
+numOfArgs = 0
+if args.updateall: numOfArgs += 1
+if args.update: numOfArgs += 1
+if args.new: numOfArgs += 1
+if numOfArgs > 1:
+    raise Exception('Please choose only one argument.')
+
+kindleBooks = Books()
+if args.updateall:
+    UpdateAll()
+elif args.update:
+    Update()
+elif args.new:
+    New()
+else:
+    parser.print_help()
+
+booksToUpdate = kindleBooks.GetBooksToUpdate()
+if len(booksToUpdate) > 0:
+    booksSkipped = []
+
+    #open X-Ray Builder GUI
+    app = Application().start(os.path.join('X-Ray Builder GUI','X-Ray Builder GUI.exe'))
+    mainWindow = app['X-Ray Builder GUI']
+    aliasesWindow = app['Aliases']
+    chaptersWindow = app['Chapters']
+    settingsWindow = app['Settings']
+
+    #minimize window
+    #mainWindow.Minimize()
+
+    #Get output directory
+    mainWindow['Button3'].Click()
+    while not settingsWindow.Exists():
+        sleep(.5)
+    outputDir = settingsWindow['Edit4'].Texts()[0]
+    settingsWindow['Button23'].Click()
+
+    #update books
+    for book in booksToUpdate:
+        try:
+            print book.bookFileName
+            print '\tUpdating ASIN and getting shelfari URL'
+            book.GetShelfariURL()
+        except Exception as e:
+            raise e
+            booksToUpdate.remove(book)
+            booksSkipped.append((book, e))
+
+        print '\tCreating X-Ray file'
+        mainWindow['Edit1'].SetEditText(book.bookLocation)
+        mainWindow['Edit2'].SetEditText(book.shelfariURL)
+        mainWindow['Button6'].Click()   #make sure Source is Shelfari
+        mainWindow['Button11'].Click()  #click create xray button
+
+        #wait for aliases window
+        while not aliasesWindow.Exists():
+            sleep(1)
+        aliasesWindow['No'].Click()
+
+        #wait for chapters window
+        while not chaptersWindow.Exists():
+            sleep(1)
+        chaptersWindow['No'].Click()
+
+        #wait for xray creation to be done
+        app.WaitCPUUsageLower(timeout=60)
+
+    #close X-Ray Builder GUI
+    app.kill_()
+
+    #move x-ray files to their respective locations
+    print 'Moving X-Ray Files to their directories'
+    xrayFiles = []
+    for dirName, subDirList, fileList in os.walk(outputDir):
+        for file in glob(os.path.join(dirName,'*.asc')):
+            xrayFiles.append(file)
+    
+    for xrayFile in xrayFiles:
+        print xrayFile
+        print okByASIN(os.path.basename(xrayFile).split('.')[1]
+        #move(xrayFile, kindleBooks.GetBookByASIN(os.path.basename(xrayFile).split('.')[1]))
+
+    for book in booksSkipped:
+        print '%s skipped because %s' % (book[0].bookFileName, book[1])
+else:
+    print "No books to update."
---
+++ kindle/books.py
@@ -0,0 +1,111 @@
+# books.py
+
+import ctypes
+import os
+from mobibook import MobiBook
+
+# Drive types
+DRIVE_UNKNOWN     = 0  # The drive type cannot be determined.
+DRIVE_NO_ROOT_DIR = 1  # The root path is invalbookID; for example, there is no volume mounted at the specified path.
+DRIVE_REMOVABLE   = 2  # The drive has removable media; for example, a floppy drive, thumb drive, or flash card reader.
+DRIVE_FIXED       = 3  # The drive has fixed media; for example, a hard disk drive or flash drive.
+DRIVE_REMOTE      = 4  # The drive is a remote (network) drive.
+DRIVE_CDROM       = 5  # The drive is a CD-ROM drive.
+DRIVE_RAMDISK     = 6  # The drive is a RAM disk.
+books_updated = []
+books_skipped = []
+
+class Books(object):
+    def __init__(self):
+        self.FindKindle()
+        self.GetBooks()
+
+    def __iter__(self):
+        for book in self.books:
+            yield book
+
+    def __str__(self):
+        string = ''
+        for book in self.books:
+            string += str(book) + '\n'
+        return string[:-1]
+
+    @property
+    def kindleDrive(self):
+        return self._kindleDrive
+
+    @kindleDrive.setter
+    def kindleDrive(self, value):
+        self._kindleDrive = value
+
+    @property
+    def books(self):
+        return self._books
+    
+    @books.setter
+    def books(self, value):
+        self._books = value
+    
+    # Return drive letter of kindle if found or None if not found
+    def FindKindle(self):
+        print "Checking for kindle..."
+        drive_info = self.GetDriveInfo()
+        removable_drives = [drive_letter for drive_letter, drive_type in drive_info if drive_type == DRIVE_REMOVABLE]
+        for drive in removable_drives:
+            for dirName, subDirList, fileList in os.walk(drive):
+                if dirName == drive + "system\.mrch":
+                    for fName in fileList:
+                        if "amzn1_account" in fName:
+                            print "Kindle found!"
+                            self.kindleDrive = drive
+                            return
+        raise KindleNotFound("Please make sure kindle is plugged in.")
+
+    # Return list of tuples mapping drive letters to drive types
+    def GetDriveInfo(self):
+        result = []
+        bitmask = ctypes.windll.kernel32.GetLogicalDrives()
+        for i in range(26):
+            bit = 2 ** i
+            if bit & bitmask:
+                drive_letter = '%s:' % chr(65 + i)
+                drive_type = ctypes.windll.kernel32.GetDriveTypeA('%s\\' % drive_letter)
+                result.append((drive_letter, drive_type))
+        return result
+
+    # Get list of books
+    def GetBooks(self):
+        books_directory = os.path.join(self.kindleDrive, 'documents')
+        self.books = []
+        index = 0
+        print "Searching for books..."
+        for dirName, subDirList, fileList in os.walk(books_directory):
+            for fName in fileList:
+                if ".mobi" in fName:
+                    index += 1
+                    self.books.append(MobiBook(os.path.join(dirName,fName), index))
+        print '%i books found.' % index
+
+    def PrintListOfBooks(self):
+        for book in self.books:
+            print '\t%i. %s' % (book.bookID, book.bookFileName)
+
+    def RemoveBooksWithXray(self):
+        for book in self.books:
+            if book.xrayExists:
+                self.books.remove(book)
+
+    def GetBooksToUpdate(self):
+        booksToUpdate = []
+        for book in self.books:
+            if book.update:
+                booksToUpdate.append(book)
+        return booksToUpdate
+
+    def GetBookByASIN(self, ASIN):
+        for book in self.books:
+            if book.ASIN == ASIN:
+                return book
+
+class KindleNotFound(Exception):
+    pass
